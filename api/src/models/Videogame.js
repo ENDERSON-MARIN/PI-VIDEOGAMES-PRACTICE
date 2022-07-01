3cc4ced48fc72691e2311f923a14601478e60c80
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-07-01 11:26:34 -0500

    Create Models Videogames, Genres

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,48 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "videogame",
+    {
+      id: {
+        //unique id created
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      released: {
+        type: DataTypes.DATEONLY,
+        allowNull: true,
+      },
+
+      rating: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+      },
+
+      background_image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      platforms: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
     },
-  });
+    { timestamps: true }
+  );
 };
