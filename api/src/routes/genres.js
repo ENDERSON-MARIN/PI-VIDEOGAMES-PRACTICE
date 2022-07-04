1b512761a26a985cafc043b8435150d87b748071
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-07-04 00:17:01 -0500

    FIXED CONTROLLERS AND ROUTES -->VIDEOGAMES

--- api/src/routes/genres.js
+++ api/src/routes/genres.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
-
+const { getAllGenres } = require("../controllers/genres");
 
 const router = Router();
+
+router.get("/", getAllGenres);
 
 module.exports = router;
